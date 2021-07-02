388c357d0c843509be35731748d20084288107ec
ギルバ <61905056+SuperMario1212@users.noreply.github.com>
2021-07-02 12:46:56 +0800

    Create 8kyu Reversed Strings.java

---
+++ 8kyu Reversed Strings.java
@@ -0,0 +1,13 @@
+
+// <My Solution>
+
+public class Kata {
+
+  public static String solution(String str) {
+    // Your code here...
+    StringBuffer reverseStr = new StringBuffer(str).reverse();
+    String newStr = reverseStr.toString();
+    return newStr;
+  }
+
+}
