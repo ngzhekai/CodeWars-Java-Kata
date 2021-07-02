5fa92d7c1594d005c34971a87a7b0adc6df6116a
ギルバ <61905056+SuperMario1212@users.noreply.github.com>
2021-07-02 16:35:03 +0800

    Added Solutions

--- 6kyu Find the odd int.java
+++ 6kyu Find the odd int.java
@@ -1,1 +1,32 @@
+// <Instructions>
 
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+// --> My Solution <--
+
++---------------------------------+
+public class FindOdd {
+  public static int findIt(int[] a) {
+    int size = a.length;
+    int res = 0;
+
+    for (int i = 0; i < size; i++) {
+      res = res ^ a[i];
+
+    }
+    return res;
+  }
+}
+
++----------------------------------+
+  
+// <Other Solution>
+
+import static java.util.Arrays.stream;
+
+public class FindOdd {
+  public static int findIt(int[] arr) {
+    return stream(arr).reduce(0, (x, y) -> x ^ y);
+  }
+}
