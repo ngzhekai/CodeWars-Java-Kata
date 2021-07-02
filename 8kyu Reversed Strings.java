dfb93a446a3acba20d6ea3d0e20d30b0bce088d8
ギルバ <61905056+SuperMario1212@users.noreply.github.com>
2021-07-02 13:02:19 +0800

    Added Solutions

--- 8kyu Reversed Strings.java
+++ 8kyu Reversed Strings.java
@@ -1,3 +1,9 @@
+/*
+ --> Instructions <--
+ Complete the solution so that it reverses the string passed into it.
+ 
+'world'  =>  'dlrow'
+*/
 
 // <My Solution>
 
@@ -8,6 +14,33 @@ public class Kata {
     StringBuffer reverseStr = new StringBuffer(str).reverse();
     String newStr = reverseStr.toString();
     return newStr;
+  }
+
+}
+
+// --> Best Solution <--
+
+public class Kata {
+
+  public static String solution(String str) {
+    return new StringBuilder(str).reverse().toString();
+  }
+
+}
+
+// --> Other Smart Solution <--
+
+public class Kata {
+
+  public static String solution(String str) {
+    // Your code here...
+    String newcad="";
+    
+    for(int i = str.length()-1; i >= 0; i--){
+      newcad += str.charAt(i);
+    }
+    
+    return newcad;
   }
 
 }
